0d53c87b16b8f6dd2eebafe48db55843925da27a
ThFischer <Fischer.Th@gmail.com>
2012-03-29 19:37:39 +0300

    catch "Maximum call stack size exceeded" Exception in array/append

--- src/array/append.js
+++ src/array/append.js
@@ -6,7 +6,14 @@ define(function () {
      * @version 0.1.0 (2012/01/31)
      */
     function append(arr1, arr2) {
-        Array.prototype.push.apply(arr1, arr2);
+        try {
+            Array.prototype.push.apply(arr1, arr2);
+        } catch (err) {
+            // "Maximum call stack size exceeded" Exceptions, append it manually:
+            for (var i = 0; i < arr2.length; i++) {
+                arr1.push(arr2[i]);
+            }
+        }
         return arr1;
     }
     return append;
