14ecf52d3d3875480c546261d88fb80f874bd502
Thomas Fischer <Fischer.Th@gmail.com>
2012-03-30 10:45:15 +0300

    remove unnecessary var lookup

--- src/lang/kindOf.js
+++ src/lang/kindOf.js
@@ -9,11 +9,9 @@ define(function () {
      * Gets the "kind" of value. (e.g. "String", "Number", etc)
      * @version 0.1.0 (2011/10/31)
      */
-    function kindOf(val) {
+    return function kindOf (val) {
         return val === null ? "Null" :
             val === UNDEF ? UNDEF_STR :
             _rKind.exec( _toString.call(val) )[1];
-    }
-
-    return kindOf;
+    };
 });
