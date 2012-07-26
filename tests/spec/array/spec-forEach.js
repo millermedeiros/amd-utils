2ea1c177030246666b4d498e680eccde7cb806f0
millermedeiros <miller@millermedeiros.com>
2012-07-26 14:03:16 -0300

    improve array/forEach spec for sparse arrays and add notes about edge cases. see #gh-57

--- tests/spec/array/spec-forEach.js
+++ tests/spec/array/spec-forEach.js
@@ -15,22 +15,66 @@ define(['src/array/forEach'], function (forEach) {
             expect( result ).toBe( 15 );
         });
 
-        it('should support sparse arrays', function () {
-            var items = new Array(6);
-            items[2] = 3;
-            items[5] = 8;
-            items[10] = undefined; // it's a trap!
+        describe('sparse arrays', function () {
 
-            var result = [];
+            it('should support sparse arrays', function () {
+                var arr1 = new Array(6);
+                arr1[2] = 3;
+                arr1[5] = 8;
+                arr1[10] = undefined; // it's a trap!
 
-            forEach(items, function(val, i, arr){
-                expect( arr ).toBe( items );
-                expect( val ).toBe( items[i] );
-                expect( i ).not.toBe( 4 ); // make sure it skips sparse items
-                result.push(val);
+                var result = [];
+
+                forEach(arr1, function(val, i, arr){
+                    expect( arr ).toBe( arr1 );
+                    expect( val ).toBe( arr1[i] );
+                    expect( i ).not.toBe( 4 ); // make sure it skips sparse items
+                    result.push(val);
+                });
+
+                expect( result[0] ).toEqual( 3 );
+                expect( result[1] ).toEqual( 8 );
+                expect( result[2] ).toEqual( undefined );
             });
 
-            expect( result ).toEqual( [3, 8, undefined] );
+            it('should support arrays with `undefined` items - this will fail on IE 6-8!!!', function () {
+                // IMPORTANT!
+                // ----------
+                // this is considered sparse on IE 6-8, there is no way to ensure
+                // proper behavior for this case everywhere!!!
+                // IE will consider the  `undefined` as "empty" so NEVER do that
+                // on your code!
+                // See: #gh-57
+
+                var arr2 = [2, void(0), 4];
+                var r2 = [];
+
+                forEach(arr2, function(val, i, arr){
+                    expect( arr ).toBe( arr2 );
+                    expect( val ).toBe( arr2[i] );
+                    r2.push(val);
+                });
+
+                expect( r2[0] ).toEqual( 2 );
+                expect( r2[1] ).toEqual( void(0) );
+                expect( r2[2] ).toEqual( 4 );
+            });
+
+            it('should support arrays with missing items', function () {
+                var arr3 = [5, ,7];
+                var r3 = [];
+
+                forEach(arr3, function(val, i, arr){
+                    expect( arr ).toBe( arr3 );
+                    expect( val ).toBe( arr3[i] );
+                    r3.push(val);
+                });
+
+                expect( r3[0] ).toEqual( 5 );
+                expect( r3[1] ).toEqual( 7 );
+                expect( r3.length ).toEqual( 2 );
+            });
+
         });
 
     });
